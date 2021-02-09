db5ef7862f54f1b6aed32f8d9fb7d9c90877974b
Martynas Gedutis <martynasgedutis@gmail.com>
2021-02-09 11:27:49 +0200

    create new endpoint for AuthorsController and add two book authors in get all method

---
+++ src/Martium.BookLovers.Api.Host/Controllers/AuthorsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Martium.BookLovers.Api.Host.Controllers
+{
+    [ApiController]
+    [Route("v1/bookLovers/authors")]
+
+    public class AuthorsController : ControllerBase
+    {
+        [HttpGet]
+
+        public ActionResult<IEnumerable<string>> GetAll()
+        {
+            return new string[] { "J. K. Rowling", "George R. R. Martin" };
+        }
+    }
+}
