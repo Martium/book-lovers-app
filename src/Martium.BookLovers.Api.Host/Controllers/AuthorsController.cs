8bebc7d02a913c1edbd4bca93dd764d2f9415466
Martynas Gedutis <martynasgedutis@gmail.com>
2021-02-09 19:10:53 +0200

    change attributes to route

--- src/Martium.BookLovers.Api.Host/Controllers/AuthorsController.cs
+++ src/Martium.BookLovers.Api.Host/Controllers/AuthorsController.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using 
 
 namespace Martium.BookLovers.Api.Host.Controllers
 {
     [ApiController]
-    [Route("v1/bookLovers/authors")]
+    [Route("v1/bookLovers")]
 
     public class AuthorsController : ControllerBase
     {
         private readonly List<string> _authors = new List<string> () { "J. K. Rowling", "George R. R. Martin" };
 
         [HttpGet]
+        [Route("authors")]
 
         public ActionResult<IEnumerable<string>> GetAll()
         {
