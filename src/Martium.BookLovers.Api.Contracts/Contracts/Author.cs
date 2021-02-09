28639afe69bc113e2b798b72e04bd07df2e20629
Martynas Gedutis <martynasgedutis@gmail.com>
2021-02-09 19:10:28 +0200

    made public class

--- src/Martium.BookLovers.Api.Contracts/Contracts/Author.cs
+++ src/Martium.BookLovers.Api.Contracts/Contracts/Author.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Martium.BookLovers.Api.Contracts.Contracts
 {
-    class Author
+    public class Author
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
