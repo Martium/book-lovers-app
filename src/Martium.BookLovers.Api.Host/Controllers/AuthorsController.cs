e7e99814ecf826f8505616162c6bacc3b6054734
Erikas Neverdauskas <erikasnever@hotmail.com>
2021-02-18 19:11:02 +0200

    call fixes

--- src/Martium.BookLovers.Api.Host/Controllers/AuthorsController.cs
+++ src/Martium.BookLovers.Api.Host/Controllers/AuthorsController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Martium.BookLovers.Api.Contracts.Response;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,7 +10,7 @@ namespace Martium.BookLovers.Api.Host.Controllers
 
     public class AuthorsController : ControllerBase
     {
-        private readonly List<AuthorReadModel> _authors = new List<AuthorReadModel>
+        private static List<AuthorReadModel> _authors = new List<AuthorReadModel>
         {
             new AuthorReadModel { Id = 1, FirstName = "Joanne", LastName =  "Rowling" },
             new AuthorReadModel { Id = 2, FirstName = "George", LastName = "Raymond Richard Martin" }
@@ -23,30 +24,37 @@ namespace Martium.BookLovers.Api.Host.Controllers
         }
 
         [HttpGet]
-        [Route("author/{id}")]
+        [Route("authors/{id}")]
         public ActionResult<AuthorReadModel> GetAuthor(int id)
         {
-            if (id > _authors.Count || id <= 0)
+            /*if (id > _authors.Count || id <= 0)
             {
                 return BadRequest($"Bad Author Id (must be in the range 1 - {_authors.Count})");
-            }
+            }*/
 
-            var author = _authors[id - 1];
+            AuthorReadModel author = _authors.FirstOrDefault(x => x.Id == id);
+
+            // ar rado ar ne ? ir jei nerado tada grazini NotFound
+
             return Ok(author);
         }
 
         [HttpPost]
-        [Route("new")]
-
-        public ActionResult Create([FromBody] AuthorReadModel authorReadModel)
+        [Route("authors")]
+        public ActionResult Create([FromBody] AuthorReadModel newAuthorRequest)
         {
-            int newId = _authors.Count + 1;
-            string firstName = "kazkas";
-            string lastName = "niekas";
+            int newId = _authors.Max(x => x.Id) + 1;
 
-            _authors.Add(new AuthorReadModel {Id = newId, FirstName = firstName, LastName = lastName});
+            var newAuthor = new AuthorReadModel
+            {
+                Id = newId, 
+                FirstName = newAuthorRequest.FirstName, 
+                LastName = newAuthorRequest.LastName
+            };
 
-            return Created("v1/bookLovers", _authors);
+            _authors.Add(newAuthor);
+
+            return CreatedAtAction(nameof(GetAuthor), new { id = newId }, newAuthor);
         }
     }
 }
