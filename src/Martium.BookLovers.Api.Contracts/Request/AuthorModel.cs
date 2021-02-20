7f5593bb8bf5ca791bd7532f9b5d861272e84e69
Martynas Gedutis <martynasgedutis@gmail.com>
2021-02-20 14:46:45 +0200

    model rename

---
+++ src/Martium.BookLovers.Api.Contracts/Request/AuthorModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Martium.BookLovers.Api.Contracts.Request
+{
+    public class NewAuthorReadModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
