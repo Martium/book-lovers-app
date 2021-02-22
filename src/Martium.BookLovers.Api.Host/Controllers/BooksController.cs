e917cf17925791b7839d0fdbad48f33703e43fa1
Martynas Gedutis <martynasgedutis@gmail.com>
2021-02-22 20:17:06 +0200

    minor changes

--- src/Martium.BookLovers.Api.Host/Controllers/BooksController.cs
+++ src/Martium.BookLovers.Api.Host/Controllers/BooksController.cs
@@ -13,6 +13,7 @@ namespace Martium.BookLovers.Api.Host.Controllers
     [Route("v1/bookLovers")]
     public class BooksController : ControllerBase
     {
+
         private static List<AuthorReadModel> _authors = new List<AuthorReadModel>
         {
             new AuthorReadModel { Id = 1, FirstName = "Joanne", LastName =  "Rowling" },
@@ -21,13 +22,13 @@ namespace Martium.BookLovers.Api.Host.Controllers
 
         private static List<BookReadModel> _books = new List<BookReadModel>()
         {
-            new BookReadModel { AuthorId = _authors[0].Id, Id = 1, BookName = "Harry Potter and the Philosopher's Stone", ReleaseYear = new DateTime(1997,06, 26 ) },
-            new BookReadModel { AuthorId = _authors[0].Id, Id = 2, BookName = "Harry Potter and the Chamber of Secrets", ReleaseYear =  new DateTime(1998, 07, 2 ) },
-            new BookReadModel { AuthorId = _authors[0].Id, Id = 3, BookName = "Harry Potter and the Prisoner of Azkaban", ReleaseYear = new DateTime(1999, 07, 8 )},
+            new BookReadModel { AuthorId = _authors[0].Id, Id = 1, BookName = "Harry Potter and the Philosopher's Stone", ReleaseYear = new DateTime(1997,06, 26) },
+            new BookReadModel { AuthorId = _authors[0].Id, Id = 2, BookName = "Harry Potter and the Chamber of Secrets", ReleaseYear =  new DateTime(1998, 07, 2) },
+            new BookReadModel { AuthorId = _authors[0].Id, Id = 3, BookName = "Harry Potter and the Prisoner of Azkaban", ReleaseYear = new DateTime(1999, 07, 8) },
 
-            new BookReadModel {AuthorId = _authors[1].Id, Id = 1, BookName = "A Game of Thrones", ReleaseYear = new DateTime(1996,08,1 )},
-            new BookReadModel { AuthorId = _authors[1].Id, Id = 2, BookName = "A Clash of Kings", ReleaseYear = new DateTime(1998,11,6 )},
-            new BookReadModel {AuthorId = _authors[1].Id, Id = 3, BookName = "A Storm of Swords", ReleaseYear = new DateTime(2000,08,8 )}
+            new BookReadModel { AuthorId = _authors[1].Id, Id = 1, BookName = "A Game of Thrones", ReleaseYear = new DateTime(1996,08,1) },
+            new BookReadModel { AuthorId = _authors[1].Id, Id = 2, BookName = "A Clash of Kings", ReleaseYear = new DateTime(1998,11,6) },
+            new BookReadModel { AuthorId = _authors[1].Id, Id = 3, BookName = "A Storm of Swords", ReleaseYear = new DateTime(2000,08,8) }
         };
 
         [HttpGet]
