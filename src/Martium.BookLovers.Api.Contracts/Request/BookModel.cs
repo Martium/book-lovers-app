2fd511838974db69ad76056907e5e0680e3e736b
Martynas Gedutis <martynasgedutis@gmail.com>
2021-02-25 14:05:46 +0200

    new request model for  book prop

---
+++ src/Martium.BookLovers.Api.Contracts/Request/BookModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Martium.BookLovers.Api.Contracts.Request
+{
+    public class BookModel
+    {
+        public string BookName { get; set; }
+        public int ReleaseYear { get; set; }
+    }
+}
