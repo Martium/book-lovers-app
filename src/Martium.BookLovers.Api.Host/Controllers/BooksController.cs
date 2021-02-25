5e3b0bb616bf4a4d42b9cdaa2db21b43c227e97d
Martynas Gedutis <martynasgedutis@gmail.com>
2021-02-25 13:03:05 +0200

    Get List method changes

--- src/Martium.BookLovers.Api.Host/Controllers/BooksController.cs
+++ src/Martium.BookLovers.Api.Host/Controllers/BooksController.cs
@@ -12,7 +12,7 @@ namespace Martium.BookLovers.Api.Host.Controllers
     [Route("v1/bookLovers")]
     public class BooksController : ControllerBase
     {
-        public static List<BookReadModel> _books = new List<BookReadModel>()
+        public static List<BookReadModel> Books = new List<BookReadModel>()
         {
             new BookReadModel { AuthorId = AuthorsController.Authors[0].Id, Id = 1, BookName = "Harry Potter and the Philosopher's Stone", ReleaseYear = 1997 },
             new BookReadModel { AuthorId = AuthorsController.Authors[0].Id, Id = 2, BookName = "Harry Potter and the Chamber of Secrets", ReleaseYear =  1998 },
@@ -28,9 +28,16 @@ namespace Martium.BookLovers.Api.Host.Controllers
         [Route("books")]
         public ActionResult<IEnumerable<AuthorReadModel>> GetList([FromQuery] int? authorId)
         {
-            // nereikia autoriaus tikrinti ir reikia atsizvelgti i tai ar yra authorId ar nera ir atitinkamai
+            if (authorId == null)
+            {
+                return Ok(Books);
+            }
+            else if (!AuthorsController.Authors.Exists(c => c.Id == authorId))
+            {
+                return NotFound("authorNotFound");
+            }
 
-            var authorBooks = _books.FindAll(c => c.AuthorId == authorId);
+            List<BookReadModel> authorBooks = Books.FindAll(c => c.AuthorId == authorId);
 
             return Ok(authorBooks);
         }
@@ -40,7 +47,7 @@ namespace Martium.BookLovers.Api.Host.Controllers
         [Route("books/{id}")]
         public ActionResult<IEnumerable<AuthorReadModel>> GetAuthorBook(int id)
         {
-            BookReadModel book = _books.FirstOrDefault(y => y.Id == id);
+            BookReadModel book = Books.FirstOrDefault(y => y.Id == id);
 
             if (book == null)
             {
