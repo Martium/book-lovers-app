83af5240cb05216fff0c30d4f3876f233b0b7c36
Martynas Gedutis <martynasgedutis@gmail.com>
2021-02-25 14:49:57 +0200

    delete unusing class in .net System

--- src/Martium.BookLovers.Api.Host/Controllers/BooksController.cs
+++ src/Martium.BookLovers.Api.Host/Controllers/BooksController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Martium.BookLovers.Api.Contracts.Request;
 using Martium.BookLovers.Api.Contracts.Response;
