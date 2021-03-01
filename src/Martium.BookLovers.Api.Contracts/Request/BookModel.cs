8495010276d1863f10d1e325dfa1d3f6b9836901
Martynas Gedutis <martynasgedutis@gmail.com>
2021-03-01 13:01:33 +0200

    add  one prop to  BookModel

--- src/Martium.BookLovers.Api.Contracts/Request/BookModel.cs
+++ src/Martium.BookLovers.Api.Contracts/Request/BookModel.cs
@@ -6,6 +6,7 @@ namespace Martium.BookLovers.Api.Contracts.Request
 {
     public class BookModel
     {
+        public int AuthorId { get; set; }
         public string BookName { get; set; }
         public int ReleaseYear { get; set; }
     }
