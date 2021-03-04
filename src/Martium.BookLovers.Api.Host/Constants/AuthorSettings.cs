baf39e5ffe083f07320eb16a802f01048eb45f7a
Martynas Gedutis <martynasgedutis@gmail.com>
2021-03-04 18:26:30 +0200

    author setiings class

---
+++ src/Martium.BookLovers.Api.Host/Constants/AuthorSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Martium.BookLovers.Api.Host.Constants
+{
+    public class AuthorSettings
+    {
+        public class Lengths
+        {
+            public static int FirstName => 100;
+            public static int LastName => 100;
+        }
+    }
+}
