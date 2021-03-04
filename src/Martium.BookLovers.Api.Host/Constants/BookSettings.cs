ee7c45b4f7abcc427260e140ae3c4ce232f53ae7
Martynas Gedutis <martynasgedutis@gmail.com>
2021-03-04 19:10:03 +0200

    create author and book table create methods

---
+++ src/Martium.BookLovers.Api.Host/Constants/BookSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Martium.BookLovers.Api.Host.Constants
+{
+    public class BookSettings
+    {
+    }
+}
--- src/Martium.BookLovers.Api.Host/Repositories/DatabaseInitializerRepository.cs
+++ src/Martium.BookLovers.Api.Host/Repositories/DatabaseInitializerRepository.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Data.SQLite;
 using System.Linq;
 using System.Threading.Tasks;
+using Martium.BookLovers.Api.Host.Constants;
 
 namespace Martium.BookLovers.Api.Host.Repositories
 {
@@ -30,6 +31,16 @@ namespace Martium.BookLovers.Api.Host.Repositories
             }
 
             SQLiteConnection.CreateFile(DatabaseConfiguration.DatabaseFile);
+
+            using (SQLiteConnection dbConnection = new SQLiteConnection(DatabaseConfiguration.ConnectionString))
+            {
+                dbConnection.Open();
+
+                CreateAuthorTable(dbConnection);
+
+                CreateBookTable(dbConnection);
+
+            }
         }
 
         private void DeleteLeftoverFilesAndFolders()
@@ -45,6 +56,49 @@ namespace Martium.BookLovers.Api.Host.Repositories
             {
                 subDirectory.Delete(true);
             }
+        }
+
+        private void CreateAuthorTable(SQLiteConnection dbConnection)
+        {
+            string dropAuthorTableQuery = GetDropTableQuery("Author");
+            SQLiteCommand dropAuthorTableCommand = new SQLiteCommand(dropAuthorTableQuery, dbConnection);
+            dropAuthorTableCommand.ExecuteNonQuery();
+
+            string createAuthorTableQuery =
+                $@"                  
+                  CREATE TABLE [Author] (
+                    [Id] [INTEGER] NOT NULL,
+                    [FirstName] [nvarchar]({AuthorSettings.Lengths.FirstName}) NOT NULL,
+                    [LastName] [nvarchar]({AuthorSettings.Lengths.LastName}) NOT NULL,
+                    UNIQUE(Id)
+                  );
+                ";
+
+            SQLiteCommand createAuthorTableCommand = new SQLiteCommand(createAuthorTableQuery, dbConnection);
+            createAuthorTableCommand.ExecuteNonQuery();
+        }
+
+        private void CreateBookTable(SQLiteConnection dbConnection)
+        {
+            string dropBookTableQuery = GetDropTableQuery("Book");
+            SQLiteCommand dropBookTableCommand = new SQLiteCommand(dropBookTableQuery,dbConnection);
+            dropBookTableCommand.ExecuteNonQuery();
+
+            string createBookTableQuery =
+                $@"                  
+                  CREATE TABLE [Book] (
+                    [AuthorId] [INTEGER] NOT NULL,
+                    [Id] [INTEGER] NOT NULL,
+                    [BookName] [nvarchar]({BookSettings.Lengths.BookName}) NOT NULL,
+                    [ReleaseYear] [INTEGER] NOT NULL,
+                    UNIQUE(AuthorId, Id)
+                  );
+                ";
+        }
+
+        private string GetDropTableQuery(string tableName)
+        {
+            return $"DROP TABLE IF EXISTS [{tableName}]";
         }
     }
 }
