d6033036e56bef798ac44424551ba8a86faf131c
Martynas Gedutis <martynasgedutis@gmail.com>
2021-03-04 17:38:53 +0200

    error cant find dapper

--- src/Martium.BookLovers.Api.Host/Repositories/DatabaseInitializerRepository.cs
+++ src/Martium.BookLovers.Api.Host/Repositories/DatabaseInitializerRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Data.SQLite;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,42 @@ namespace Martium.BookLovers.Api.Host.Repositories
 {
     public class DatabaseInitializerRepository
     {
+        public void InitializeDatabaseIfNotExist()
+        {
+            if (File.Exists(DatabaseConfiguration.DatabaseFile))
+            {
+#if DEBUG
+
+#else
+                return;
+#endif
+            }
+
+            if (!Directory.Exists(DatabaseConfiguration.DatabaseFolder))
+            {
+                Directory.CreateDirectory(DatabaseConfiguration.DatabaseFolder);
+            }
+            else
+            {
+                DeleteLeftoverFilesAndFolders();
+            }
+
+            SQLiteConnection.CreateFile(DatabaseConfiguration.DatabaseFile);
+        }
+
+        private void DeleteLeftoverFilesAndFolders()
+        {
+            var directory = new DirectoryInfo(DatabaseConfiguration.DatabaseFolder);
+
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                file.Delete();
+            }
+
+            foreach (DirectoryInfo subDirectory in directory.GetDirectories())
+            {
+                subDirectory.Delete(true);
+            }
+        }
     }
 }
