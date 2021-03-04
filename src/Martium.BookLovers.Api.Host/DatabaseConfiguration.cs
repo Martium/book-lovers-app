9c7611335c9ef6829202e0ecc19218c8dd05a344
Martynas Gedutis <martynasgedutis@gmail.com>
2021-03-04 17:16:45 +0200

    DatabaseConfiguration class to create path where database will be & ConnectionString initialize

---
+++ src/Martium.BookLovers.Api.Host/DatabaseConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Martium.BookLovers.Api.Host
+{
+    public class DatabaseConfiguration
+    {
+        private static readonly string DatabaseName = "BookLovers";
+
+        public static string DatabaseFolder => $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\Database";
+        public static string DatabaseFile => $"{DatabaseFolder}\\{DatabaseName}.db";
+        public static string ConnectionString => $"Data Source={DatabaseFile};Version=3;UseUTF16Encoding=True;";
+    }
+}
