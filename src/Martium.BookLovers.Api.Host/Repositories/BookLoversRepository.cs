3edf049bc165a0876287f46b00d78baf5c151de6
Martynas Gedutis <martynasgedutis@gmail.com>
2021-03-05 17:56:09 +0200

    create get all authors method from database table author

---
+++ src/Martium.BookLovers.Api.Host/Repositories/BookLoversRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using Dapper;
+using Martium.BookLovers.Api.Contracts.Response;
+
+namespace Martium.BookLovers.Api.Host.Repositories
+{
+    public class BookLoversRepository
+    {
+        public List<AuthorReadModel> GetAuthors()
+        {
+            using (SQLiteConnection dbConnection = new SQLiteConnection(DatabaseConfiguration.ConnectionString))
+            {
+                dbConnection.Open();
+
+                string getExistingAuthors =
+                    @"SELECT *
+                      FROM Author
+                    ";
+
+                IEnumerable<AuthorReadModel> getAllAuthors = dbConnection.Query<AuthorReadModel>(getExistingAuthors);
+                return getAllAuthors.ToList();
+            }
+        }
+    }
+}
