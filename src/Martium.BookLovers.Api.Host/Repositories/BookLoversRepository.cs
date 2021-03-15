aaae287341fa7350b037f7d74d4a8974dfb2f862
Martynas Gedutis <martynasgedutis@gmail.com>
2021-03-15 18:12:36 +0200

    create two new methods to get new id for new author &  CreateNewAuthor

--- src/Martium.BookLovers.Api.Host/Repositories/BookLoversRepository.cs
+++ src/Martium.BookLovers.Api.Host/Repositories/BookLoversRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Linq;
 using Dapper;
+using Martium.BookLovers.Api.Contracts.Request;
 using Martium.BookLovers.Api.Contracts.Response;
 
 namespace Martium.BookLovers.Api.Host.Repositories
@@ -54,6 +55,51 @@ namespace Martium.BookLovers.Api.Host.Repositories
                     AuthorReadModel authorNotExists = null;
                     return authorNotExists;
                 }
+            }
+        }
+
+        private int GetIdForNewAuthor()
+        {
+            using (SQLiteConnection dbConnection = new SQLiteConnection(DatabaseConfiguration.ConnectionString))
+            {
+                dbConnection.Open();
+
+                string getBiggestIdNumber =
+                    @"SELECT
+                        MAX(A.Id)
+                      FROM Authors A
+                     ";
+
+
+                int? biggestAuthorId = dbConnection.QuerySingle<int?>(getBiggestIdNumber) ?? 0;
+
+                return biggestAuthorId.Value + 1;
+            }
+        }
+
+        public bool CreateNewAuthor(AuthorModel newAuthor)
+        {
+            int newAuthorId = GetIdForNewAuthor();
+
+            using (SQLiteConnection dbConnection = new SQLiteConnection(DatabaseConfiguration.ConnectionString))
+            {
+                dbConnection.Open();
+
+                string createNewAuthor =
+                    @"INSERT INTO 'Authors'
+                        VALUES (
+                            @Id, @FirstName, @LastName 
+                        )
+                     ";
+
+                object queryParameters = new
+                {
+                    Id = newAuthorId, newAuthor.FirstName, newAuthor.LastName
+                };
+
+                int affectedRows = dbConnection.Execute(createNewAuthor, queryParameters);
+
+                return affectedRows == 1;
 
             }
         }
