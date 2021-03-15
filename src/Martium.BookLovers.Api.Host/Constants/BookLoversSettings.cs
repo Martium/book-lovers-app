1314a002d431c4ce520f1e60ee62f395509348ab
Martynas Gedutis <martynasgedutis@gmail.com>
2021-03-15 16:54:16 +0200

    redone author settings to BookLoversSettings

--- src/Martium.BookLovers.Api.Host/Constants/BookLoversSettings.cs
+++ src/Martium.BookLovers.Api.Host/Constants/BookLoversSettings.cs
@@ -5,12 +5,17 @@ using System.Threading.Tasks;
 
 namespace Martium.BookLovers.Api.Host.Constants
 {
-    public class AuthorSettings
+    public class BookLoversSettings
     {
-        public class Lengths
+        public class AuthorLengths
         {
             public static int FirstName => 100;
             public static int LastName => 100;
+        }
+
+        public class BookLengths
+        {
+            public static int BookName => 100;
         }
     }
 }
