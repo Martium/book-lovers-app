84e46e6c7d2e6d4fa92e18a497fcc9fbbab2e7ac
Martynas Gedutis <martynasgedutis@gmail.com>
2021-03-23 17:54:07 +0200

    create two methods to get book by id & check is book ir exists

--- src/Martium.BookLovers.Api.Host/Repositories/BookRepository.cs
+++ src/Martium.BookLovers.Api.Host/Repositories/BookRepository.cs
@@ -26,7 +26,7 @@ namespace Martium.BookLovers.Api.Host.Repositories
             }
         }
 
-        public List<BookReadModel> GetAuthorsBooks(int authorId)
+        public List<BookReadModel> GetAllAuthorsBooks(int authorId)
         {
             using (SQLiteConnection dbConnection = new SQLiteConnection(DatabaseConfiguration.ConnectionString))
             {
@@ -45,6 +45,30 @@ namespace Martium.BookLovers.Api.Host.Repositories
 
                 IEnumerable<BookReadModel> getAllAuthorsBooks = dbConnection.Query<BookReadModel>(getExistingBooksCommand, queryParameters);
                 return getAllAuthorsBooks.ToList();
+            }
+        }
+
+        public BookReadModel GetBookById(int id)
+        {
+            using (SQLiteConnection dbConnection = new SQLiteConnection(DatabaseConfiguration.ConnectionString))
+            {
+                dbConnection.Open();
+
+                string getExistingBookCommand =
+                    @"SELECT
+                        B.Id , B.AuthorId, B.BookName , B.ReleaseYear
+                      FROM Books B
+                      WHERE
+                        B.Id = @Id;
+                     ";
+
+                object queryParameter = new
+                {
+                    Id = id
+                };
+
+                BookReadModel getBook = dbConnection.QuerySingle<BookReadModel>(getExistingBookCommand, queryParameter);
+                return getBook;
             }
         }
 
@@ -72,6 +96,38 @@ namespace Martium.BookLovers.Api.Host.Repositories
                 object queryParameters = new
                 {
                     AuthorId = authorId
+                };
+
+                bool isIdExists = dbConnection.ExecuteScalar<bool>(chechAuthorIdCommand, queryParameters);
+
+                return isIdExists;
+            }
+        }
+
+        public bool CheckBookId(int id)
+        {
+            using (SQLiteConnection dbConnection = new SQLiteConnection(DatabaseConfiguration.ConnectionString))
+            {
+                dbConnection.Open();
+
+                string chechAuthorIdCommand =
+                    @"SELECT
+                        B.Id
+                      FROM Books B
+                      WHERE
+                        EXISTS (
+                            SELECT
+                                1
+                            FROM
+                                Books
+                            WHERE
+                                Id = @Id
+                        );
+                     ";
+
+                object queryParameters = new
+                {
+                    Id = id
                 };
 
                 bool isIdExists = dbConnection.ExecuteScalar<bool>(chechAuthorIdCommand, queryParameters);
