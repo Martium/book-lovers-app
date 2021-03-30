2ff906bab64af0d5d38f74074a5dc1a3a45f7cd9
Martynas Gedutis <martynasgedutis@gmail.com>
2021-03-30 21:54:05 +0300

    AuthorService class all api methods logic here Delete, Put, Post and Get

---
+++ src/Martium.BookLovers.Api.Host/Services/AuthorService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Martium.BookLovers.Api.Contracts.Request;
+using Martium.BookLovers.Api.Contracts.Response;
+using Martium.BookLovers.Api.Host.Errors.Exceptions;
+using Martium.BookLovers.Api.Host.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Martium.BookLovers.Api.Host.Services
+{
+    public class AuthorService : ControllerBase
+    {
+        private readonly AuthorRepository _authorRepository = new AuthorRepository();
+
+        public List<AuthorReadModel> GetAuthorsList()
+        {
+            List<AuthorReadModel> authors = _authorRepository.GetAuthors();
+
+            return authors;
+        }
+
+        public AuthorReadModel GetAuthorById(int id)
+        {
+            AuthorReadModel author = _authorRepository.GetAuthorById(id);
+
+            if (author == null)
+            {
+                throw new AuthorNotFoundException();
+            }
+
+            return author;
+        }
+
+        public int CreateNewAuthorId(AuthorModel author)
+        {
+            int id = _authorRepository.CreateNewAuthor(author);
+
+            return id;
+        }
+
+        public AuthorReadModel UpdateAuthor(AuthorModel authorUpdate, int id)
+        {
+            AuthorReadModel existingAuthor = _authorRepository.GetAuthorById(id);
+
+            if (existingAuthor == null)
+            {
+                throw new AuthorNotFoundException();
+            }
+
+            _authorRepository.UpdateAuthorById(id, authorUpdate);
+
+            AuthorReadModel updatedAuthor = _authorRepository.GetAuthorById(id);
+
+            return updatedAuthor;
+        }
+
+        public void DeleteAuthor(int id)
+        {
+            AuthorReadModel existingAuthor = _authorRepository.GetAuthorById(id);
+
+            if (existingAuthor == null)
+            {
+                throw new AuthorNotFoundException();
+            }
+
+            _authorRepository.DeleteAuthorById(id);
+        }
+    }
+}
