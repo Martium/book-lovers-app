4bdd2ca25fb08cd84e46bff1a81c52244f67d4a0
Martynas Gedutis <martynasgedutis@gmail.com>
2021-03-30 22:44:20 +0300

    error class AuthorBadRequestException

---
+++ src/Martium.BookLovers.Api.Host/Errors/Exceptions/AuthorBadRequestException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Martium.BookLovers.Api.Host.Errors.Exceptions
+{
+    public class AuthorBadRequestException : BadRequestException
+    {
+        public AuthorBadRequestException() : base("invalidAuthorIdProvided", "Invalid Author Id was provided.")
+        {
+            
+        }
+    }
+}
