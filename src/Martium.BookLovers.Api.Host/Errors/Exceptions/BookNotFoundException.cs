012a21ae008409d0f0178b1888de1acd1c6a4715
Martynas Gedutis <martynasgedutis@gmail.com>
2021-03-30 22:14:33 +0300

    error Book not found class

---
+++ src/Martium.BookLovers.Api.Host/Errors/Exceptions/BookNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Martium.BookLovers.Api.Host.Errors.Exceptions
+{
+    public class BookNotFoundException : NotFoundException
+    {
+        public BookNotFoundException() : base("bookNotFound", "Book was not found.")
+        {
+            
+        }
+    }
+}
