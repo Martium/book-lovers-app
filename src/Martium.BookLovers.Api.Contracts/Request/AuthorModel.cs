d1c9c794c1ad74e57a0a4185a4b2ded0de41ac13
Martynas Gedutis <martynasgedutis@gmail.com>
2021-04-20 12:56:49 +0300

    remove unnecessary

--- src/Martium.BookLovers.Api.Contracts/Request/AuthorModel.cs
+++ src/Martium.BookLovers.Api.Contracts/Request/AuthorModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Text;
-
-namespace Martium.BookLovers.Api.Contracts.Request
+﻿namespace Martium.BookLovers.Api.Contracts.Request
 {
     public class AuthorModel
     {
