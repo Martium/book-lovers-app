679b713e6676acc77e24d3585f04980c8fab9660
Erikas Neverdauskas <erikasnever@hotmail.com>
2021-04-29 20:48:36 +0300

    added get example & web project

---
+++ src/Martium.BookLovers.Api.Client/AuthorsApiClient.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Martium.BookLovers.Api.Contracts.Response;
+using RestSharp;
+
+namespace Martium.BookLovers.Api.Client
+{
+    public class AuthorsApiClient
+    {
+        private readonly RestClient _restClient;
+
+        public AuthorsApiClient()
+        {
+            _restClient = new RestClient("http://localhost:5000/");
+        }
+
+        public List<AuthorReadModel> GetAuthors()
+        {
+            var request = new RestRequest(Method.GET)
+            {
+                Resource = "v1/bookLovers/authors"
+            };
+
+            var response = _restClient.Execute<List<AuthorReadModel>>(request);
+
+            return response.IsSuccessful 
+                ? response.Data 
+                : new List<AuthorReadModel>();
+        }
+    }
+}
