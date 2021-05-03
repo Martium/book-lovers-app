7e6ae03afa6c5678a1de51d8d117adc8fe63bfca
Martynas Gedutis <martynasgedutis@gmail.com>
2021-05-03 20:20:43 +0300

    MessageDialogService introduce to show error message box

---
+++ src/Martium.BookLovers.Web/Service/MessageDialogService.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Martium.BookLovers.Web.Service
+{
+    public class MessageDialogService
+    {
+        public void ShowErrorMessage(string errorMessage)
+        {
+            MessageBox.Show(errorMessage);
+        }
+    }
+}
