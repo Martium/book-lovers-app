505785d0de188cf2962fc3cf6a5a28c54f3f6d8c
Martynas Gedutis <martynasgedutis@gmail.com>
2021-05-07 18:21:56 +0300

    create for book combo boxes, text boxes & labels

--- src/Martium.BookLovers.Web/AuthorForm.Designer.cs
+++ src/Martium.BookLovers.Web/AuthorForm.Designer.cs
@@ -45,6 +45,24 @@
             this.CreateNewAuthorButton = new System.Windows.Forms.Button();
             this.UpdateAuthorButton = new System.Windows.Forms.Button();
             this.DeleteButton = new System.Windows.Forms.Button();
+            this.AuthorInfoLabel = new System.Windows.Forms.Label();
+            this.BooksInfoLabel = new System.Windows.Forms.Label();
+            this.BookIdLabel = new System.Windows.Forms.Label();
+            this.ReleaseYearLabel = new System.Windows.Forms.Label();
+            this.BookNameLabel = new System.Windows.Forms.Label();
+            this.BookAuthorIdLabel = new System.Windows.Forms.Label();
+            this.BookIdTextBox = new System.Windows.Forms.TextBox();
+            this.BookAuthorIdTextBox = new System.Windows.Forms.TextBox();
+            this.BookNameTextBox = new System.Windows.Forms.TextBox();
+            this.ReleaseYearTextBox = new System.Windows.Forms.TextBox();
+            this.BookIdListLabel = new System.Windows.Forms.Label();
+            this.BookAuthorIdListLabel = new System.Windows.Forms.Label();
+            this.BookIdComboBox = new System.Windows.Forms.ComboBox();
+            this.BookAuthorIdComboBox = new System.Windows.Forms.ComboBox();
+            this.BookNameListLabel = new System.Windows.Forms.Label();
+            this.BookNameComboBox = new System.Windows.Forms.ComboBox();
+            this.BookReleaseYearListLabel = new System.Windows.Forms.Label();
+            this.ReleaseYearComboBox = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // AuthorIdComboBox
@@ -86,7 +104,7 @@
             // 
             // AuthorIdTextBox
             // 
-            this.AuthorIdTextBox.Location = new System.Drawing.Point(109, 21);
+            this.AuthorIdTextBox.Location = new System.Drawing.Point(109, 47);
             this.AuthorIdTextBox.Name = "AuthorIdTextBox";
             this.AuthorIdTextBox.Size = new System.Drawing.Size(114, 20);
             this.AuthorIdTextBox.TabIndex = 4;
@@ -94,14 +112,14 @@
             // 
             // AuthorFirstNameTextBox
             // 
-            this.AuthorFirstNameTextBox.Location = new System.Drawing.Point(109, 59);
+            this.AuthorFirstNameTextBox.Location = new System.Drawing.Point(109, 83);
             this.AuthorFirstNameTextBox.Name = "AuthorFirstNameTextBox";
             this.AuthorFirstNameTextBox.Size = new System.Drawing.Size(197, 20);
             this.AuthorFirstNameTextBox.TabIndex = 5;
             // 
             // AuthorLastNameTextBox
             // 
-            this.AuthorLastNameTextBox.Location = new System.Drawing.Point(109, 93);
+            this.AuthorLastNameTextBox.Location = new System.Drawing.Point(109, 118);
             this.AuthorLastNameTextBox.Name = "AuthorLastNameTextBox";
             this.AuthorLastNameTextBox.Size = new System.Drawing.Size(197, 20);
             this.AuthorLastNameTextBox.TabIndex = 6;
@@ -109,7 +127,7 @@
             // AuthorIdLabel
             // 
             this.AuthorIdLabel.AutoSize = true;
-            this.AuthorIdLabel.Location = new System.Drawing.Point(39, 24);
+            this.AuthorIdLabel.Location = new System.Drawing.Point(52, 50);
             this.AuthorIdLabel.Name = "AuthorIdLabel";
             this.AuthorIdLabel.Size = new System.Drawing.Size(50, 13);
             this.AuthorIdLabel.TabIndex = 7;
@@ -118,7 +136,7 @@
             // AuthorFirstNameLabel
             // 
             this.AuthorFirstNameLabel.AutoSize = true;
-            this.AuthorFirstNameLabel.Location = new System.Drawing.Point(12, 62);
+            this.AuthorFirstNameLabel.Location = new System.Drawing.Point(11, 86);
             this.AuthorFirstNameLabel.Name = "AuthorFirstNameLabel";
             this.AuthorFirstNameLabel.Size = new System.Drawing.Size(91, 13);
             this.AuthorFirstNameLabel.TabIndex = 8;
@@ -127,7 +145,7 @@
             // AuthorLastNameLabel
             // 
             this.AuthorLastNameLabel.AutoSize = true;
-            this.AuthorLastNameLabel.Location = new System.Drawing.Point(12, 96);
+            this.AuthorLastNameLabel.Location = new System.Drawing.Point(11, 121);
             this.AuthorLastNameLabel.Name = "AuthorLastNameLabel";
             this.AuthorLastNameLabel.Size = new System.Drawing.Size(92, 13);
             this.AuthorLastNameLabel.TabIndex = 9;
@@ -200,11 +218,181 @@
             this.DeleteButton.UseVisualStyleBackColor = true;
             this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
             // 
+            // AuthorInfoLabel
+            // 
+            this.AuthorInfoLabel.AutoSize = true;
+            this.AuthorInfoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AuthorInfoLabel.Location = new System.Drawing.Point(12, 9);
+            this.AuthorInfoLabel.Name = "AuthorInfoLabel";
+            this.AuthorInfoLabel.Size = new System.Drawing.Size(72, 20);
+            this.AuthorInfoLabel.TabIndex = 17;
+            this.AuthorInfoLabel.Text = "Authors";
+            // 
+            // BooksInfoLabel
+            // 
+            this.BooksInfoLabel.AutoSize = true;
+            this.BooksInfoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BooksInfoLabel.Location = new System.Drawing.Point(12, 230);
+            this.BooksInfoLabel.Name = "BooksInfoLabel";
+            this.BooksInfoLabel.Size = new System.Drawing.Size(59, 20);
+            this.BooksInfoLabel.TabIndex = 18;
+            this.BooksInfoLabel.Text = "Books";
+            // 
+            // BookIdLabel
+            // 
+            this.BookIdLabel.AutoSize = true;
+            this.BookIdLabel.Location = new System.Drawing.Point(59, 273);
+            this.BookIdLabel.Name = "BookIdLabel";
+            this.BookIdLabel.Size = new System.Drawing.Size(44, 13);
+            this.BookIdLabel.TabIndex = 19;
+            this.BookIdLabel.Text = "Book Id";
+            // 
+            // ReleaseYearLabel
+            // 
+            this.ReleaseYearLabel.AutoSize = true;
+            this.ReleaseYearLabel.Location = new System.Drawing.Point(8, 358);
+            this.ReleaseYearLabel.Name = "ReleaseYearLabel";
+            this.ReleaseYearLabel.Size = new System.Drawing.Size(94, 13);
+            this.ReleaseYearLabel.TabIndex = 20;
+            this.ReleaseYearLabel.Text = "Book release Year";
+            // 
+            // BookNameLabel
+            // 
+            this.BookNameLabel.AutoSize = true;
+            this.BookNameLabel.Location = new System.Drawing.Point(39, 332);
+            this.BookNameLabel.Name = "BookNameLabel";
+            this.BookNameLabel.Size = new System.Drawing.Size(63, 13);
+            this.BookNameLabel.TabIndex = 21;
+            this.BookNameLabel.Text = "Book Name";
+            // 
+            // BookAuthorIdLabel
+            // 
+            this.BookAuthorIdLabel.AutoSize = true;
+            this.BookAuthorIdLabel.Location = new System.Drawing.Point(53, 303);
+            this.BookAuthorIdLabel.Name = "BookAuthorIdLabel";
+            this.BookAuthorIdLabel.Size = new System.Drawing.Size(50, 13);
+            this.BookAuthorIdLabel.TabIndex = 22;
+            this.BookAuthorIdLabel.Text = "Author Id";
+            // 
+            // BookIdTextBox
+            // 
+            this.BookIdTextBox.Location = new System.Drawing.Point(109, 270);
+            this.BookIdTextBox.Name = "BookIdTextBox";
+            this.BookIdTextBox.Size = new System.Drawing.Size(114, 20);
+            this.BookIdTextBox.TabIndex = 23;
+            // 
+            // BookAuthorIdTextBox
+            // 
+            this.BookAuthorIdTextBox.Location = new System.Drawing.Point(109, 300);
+            this.BookAuthorIdTextBox.Name = "BookAuthorIdTextBox";
+            this.BookAuthorIdTextBox.Size = new System.Drawing.Size(114, 20);
+            this.BookAuthorIdTextBox.TabIndex = 24;
+            // 
+            // BookNameTextBox
+            // 
+            this.BookNameTextBox.Location = new System.Drawing.Point(108, 329);
+            this.BookNameTextBox.Name = "BookNameTextBox";
+            this.BookNameTextBox.Size = new System.Drawing.Size(198, 20);
+            this.BookNameTextBox.TabIndex = 25;
+            // 
+            // ReleaseYearTextBox
+            // 
+            this.ReleaseYearTextBox.Location = new System.Drawing.Point(108, 358);
+            this.ReleaseYearTextBox.Name = "ReleaseYearTextBox";
+            this.ReleaseYearTextBox.Size = new System.Drawing.Size(114, 20);
+            this.ReleaseYearTextBox.TabIndex = 26;
+            // 
+            // BookIdListLabel
+            // 
+            this.BookIdListLabel.AutoSize = true;
+            this.BookIdListLabel.Location = new System.Drawing.Point(13, 434);
+            this.BookIdListLabel.Name = "BookIdListLabel";
+            this.BookIdListLabel.Size = new System.Drawing.Size(63, 13);
+            this.BookIdListLabel.TabIndex = 27;
+            this.BookIdListLabel.Text = "Book Id List";
+            // 
+            // BookAuthorIdListLabel
+            // 
+            this.BookAuthorIdListLabel.AutoSize = true;
+            this.BookAuthorIdListLabel.Location = new System.Drawing.Point(182, 434);
+            this.BookAuthorIdListLabel.Name = "BookAuthorIdListLabel";
+            this.BookAuthorIdListLabel.Size = new System.Drawing.Size(69, 13);
+            this.BookAuthorIdListLabel.TabIndex = 28;
+            this.BookAuthorIdListLabel.Text = "Author Id List";
+            // 
+            // BookIdComboBox
+            // 
+            this.BookIdComboBox.FormattingEnabled = true;
+            this.BookIdComboBox.Location = new System.Drawing.Point(82, 431);
+            this.BookIdComboBox.Name = "BookIdComboBox";
+            this.BookIdComboBox.Size = new System.Drawing.Size(77, 21);
+            this.BookIdComboBox.TabIndex = 29;
+            // 
+            // BookAuthorIdComboBox
+            // 
+            this.BookAuthorIdComboBox.FormattingEnabled = true;
+            this.BookAuthorIdComboBox.Location = new System.Drawing.Point(257, 431);
+            this.BookAuthorIdComboBox.Name = "BookAuthorIdComboBox";
+            this.BookAuthorIdComboBox.Size = new System.Drawing.Size(77, 21);
+            this.BookAuthorIdComboBox.TabIndex = 30;
+            // 
+            // BookNameListLabel
+            // 
+            this.BookNameListLabel.AutoSize = true;
+            this.BookNameListLabel.Location = new System.Drawing.Point(366, 434);
+            this.BookNameListLabel.Name = "BookNameListLabel";
+            this.BookNameListLabel.Size = new System.Drawing.Size(82, 13);
+            this.BookNameListLabel.TabIndex = 31;
+            this.BookNameListLabel.Text = "Book Name List";
+            // 
+            // BookNameComboBox
+            // 
+            this.BookNameComboBox.FormattingEnabled = true;
+            this.BookNameComboBox.Location = new System.Drawing.Point(454, 431);
+            this.BookNameComboBox.Name = "BookNameComboBox";
+            this.BookNameComboBox.Size = new System.Drawing.Size(243, 21);
+            this.BookNameComboBox.TabIndex = 32;
+            // 
+            // BookReleaseYearListLabel
+            // 
+            this.BookReleaseYearListLabel.AutoSize = true;
+            this.BookReleaseYearListLabel.Location = new System.Drawing.Point(725, 434);
+            this.BookReleaseYearListLabel.Name = "BookReleaseYearListLabel";
+            this.BookReleaseYearListLabel.Size = new System.Drawing.Size(111, 13);
+            this.BookReleaseYearListLabel.TabIndex = 33;
+            this.BookReleaseYearListLabel.Text = "Book release year List";
+            // 
+            // ReleaseYearComboBox
+            // 
+            this.ReleaseYearComboBox.FormattingEnabled = true;
+            this.ReleaseYearComboBox.Location = new System.Drawing.Point(842, 431);
+            this.ReleaseYearComboBox.Name = "ReleaseYearComboBox";
+            this.ReleaseYearComboBox.Size = new System.Drawing.Size(77, 21);
+            this.ReleaseYearComboBox.TabIndex = 34;
+            // 
             // AuthorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(968, 450);
+            this.ClientSize = new System.Drawing.Size(995, 539);
+            this.Controls.Add(this.ReleaseYearComboBox);
+            this.Controls.Add(this.BookReleaseYearListLabel);
+            this.Controls.Add(this.BookNameComboBox);
+            this.Controls.Add(this.BookNameListLabel);
+            this.Controls.Add(this.BookAuthorIdComboBox);
+            this.Controls.Add(this.BookIdComboBox);
+            this.Controls.Add(this.BookAuthorIdListLabel);
+            this.Controls.Add(this.BookIdListLabel);
+            this.Controls.Add(this.ReleaseYearTextBox);
+            this.Controls.Add(this.BookNameTextBox);
+            this.Controls.Add(this.BookAuthorIdTextBox);
+            this.Controls.Add(this.BookIdTextBox);
+            this.Controls.Add(this.BookAuthorIdLabel);
+            this.Controls.Add(this.BookNameLabel);
+            this.Controls.Add(this.ReleaseYearLabel);
+            this.Controls.Add(this.BookIdLabel);
+            this.Controls.Add(this.BooksInfoLabel);
+            this.Controls.Add(this.AuthorInfoLabel);
             this.Controls.Add(this.DeleteButton);
             this.Controls.Add(this.UpdateAuthorButton);
             this.Controls.Add(this.CreateNewAuthorButton);
@@ -250,6 +438,24 @@
         private System.Windows.Forms.Button CreateNewAuthorButton;
         private System.Windows.Forms.Button UpdateAuthorButton;
         private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Label AuthorInfoLabel;
+        private System.Windows.Forms.Label BooksInfoLabel;
+        private System.Windows.Forms.Label BookIdLabel;
+        private System.Windows.Forms.Label ReleaseYearLabel;
+        private System.Windows.Forms.Label BookNameLabel;
+        private System.Windows.Forms.Label BookAuthorIdLabel;
+        private System.Windows.Forms.TextBox BookIdTextBox;
+        private System.Windows.Forms.TextBox BookAuthorIdTextBox;
+        private System.Windows.Forms.TextBox BookNameTextBox;
+        private System.Windows.Forms.TextBox ReleaseYearTextBox;
+        private System.Windows.Forms.Label BookIdListLabel;
+        private System.Windows.Forms.Label BookAuthorIdListLabel;
+        private System.Windows.Forms.ComboBox BookIdComboBox;
+        private System.Windows.Forms.ComboBox BookAuthorIdComboBox;
+        private System.Windows.Forms.Label BookNameListLabel;
+        private System.Windows.Forms.ComboBox BookNameComboBox;
+        private System.Windows.Forms.Label BookReleaseYearListLabel;
+        private System.Windows.Forms.ComboBox ReleaseYearComboBox;
     }
 }
 
