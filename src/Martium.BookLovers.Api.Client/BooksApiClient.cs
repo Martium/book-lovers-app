222e9fc78220de7c57924e157ce840ddf18df8ee
Martynas Gedutis <martynasgedutis@gmail.com>
2021-05-07 21:21:01 +0300

    books api client class and method get all books by id  & get all authors books if author id passed

---
+++ src/Martium.BookLovers.Api.Client/BooksApiClient.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Martium.BookLovers.Api.Contracts.Response;
+using RestSharp;
+
+namespace Martium.BookLovers.Api.Client
+{
+    public class BooksApiClient
+    {
+        private readonly RestClient _restClient;
+
+        public BooksApiClient()
+        {
+            _restClient = new RestClient("http://localhost:5000/");
+        }
+
+        public List<BookReadModel> GetBooks(int? authorId)
+        {
+            var request = new RestRequest(Method.GET)
+            {
+                Resource = $"v1/bookLovers/books?authorId={authorId}"
+            };
+
+            var response = _restClient.Execute<List<BookReadModel>>(request);
+
+            return response.IsSuccessful ? response.Data : new List<BookReadModel>();
+
+        }
+    }
+}
