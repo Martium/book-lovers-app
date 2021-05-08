2f07a4e0b16ee673d96d4791ce39a86aa1987230
Martynas Gedutis <martynasgedutis@gmail.com>
2021-05-08 20:49:57 +0300

    create new error message logic

---
+++ src/Martium.BookLovers.Web/Service/ErrorMessage.cs
@@ -0,0 +1,37 @@
+﻿namespace Martium.BookLovers.Web.Service
+{
+    public class ErrorMessage
+    {
+       public string AuthorIdMustBeProvided = "Author id must be provided";
+
+       public string BookIdMustBeProvided = "Book id must be provided";
+
+       public string BadAuthorId = "Author id cant be found";
+
+       public string ThereIsNoAuthors = "There is no Authors in DataBase";
+
+       public string TextMustBeInteger = "text must be number (integer type)";
+
+       public string AuthorFirstNameMustBeProvided = "Author First name must be provided";
+
+       public string AuthorLastNameMustBeProvided = "Author Last name must be provided";
+
+       public string BookNotFound = "Book not found";
+
+       public string BookNameMustBeProvided = "Book name must be provided";
+
+       public string YearMustBeProvided = "Year must be provided";
+
+       public string AuthorWasNotUpdated = "Author was not updated";
+
+       public string AuthorWasDeletedBeforeOrNotExitsAtAll = "Cant find Author( was deleted before or not exits at all)";
+
+       public string NewBookWasNotCreated = "New Book was not created";
+
+       public string BookWasNotUpdated = "Book was not updated";
+
+       public string NewAuthorWasNotCreated = "New Author was not Created";
+
+       public string BookWasDeletedBeforeOrNotExitsAtAll = "Cant find Book( was deleted before or not exits at all)";
+    }
+}
--- src/Martium.BookLovers.Web/Service/MessageDialogService.cs
+++ src/Martium.BookLovers.Web/Service/MessageDialogService.cs
@@ -1,17 +1,77 @@
 ﻿using System.Windows.Forms;
+using Martium.BookLovers.Web.Enums;
 
 namespace Martium.BookLovers.Web.Service
 {
     public class MessageDialogService
     {
-        public void ShowErrorMessage(string errorMessage)
+        private readonly ErrorMessage _errorMessage;
+
+        public MessageDialogService()
         {
-            MessageBox.Show(errorMessage);
+            _errorMessage = new ErrorMessage();
+        }
+
+        public void ShowErrorMessage(ErrorsTypes errorsTypes)
+        {
+            switch (errorsTypes)
+            {
+                case ErrorsTypes.AuthorIdMustBeProvided:
+                    MessageBox.Show(_errorMessage.AuthorIdMustBeProvided);
+                    break;
+                case ErrorsTypes.BookIdMustBeProvided:
+                    MessageBox.Show(_errorMessage.BookIdMustBeProvided);
+                    break;
+                case ErrorsTypes.BadAuthorId:
+                    MessageBox.Show(_errorMessage.BadAuthorId);
+                    break;
+                case ErrorsTypes.ThereIsNoAuthors:
+                    MessageBox.Show(_errorMessage.ThereIsNoAuthors);
+                    break;
+                case ErrorsTypes.TextMustBeInteger:
+                    MessageBox.Show(_errorMessage.TextMustBeInteger);
+                    break;
+                case ErrorsTypes.AuthorFirstNameMustBeProvided:
+                    MessageBox.Show(_errorMessage.AuthorFirstNameMustBeProvided);
+                    break;
+                case ErrorsTypes.AuthorLastNameMustBeProvided:
+                    MessageBox.Show(_errorMessage.AuthorLastNameMustBeProvided);
+                    break;
+                case ErrorsTypes.BookNotFound:
+                    MessageBox.Show(_errorMessage.BookNotFound);
+                    break;
+                case ErrorsTypes.BookNameMustBeProvided:
+                    MessageBox.Show(_errorMessage.BookNameMustBeProvided);
+                    break;
+                case ErrorsTypes.YearMustBeProvided:
+                    MessageBox.Show(_errorMessage.YearMustBeProvided);
+                    break;
+                case ErrorsTypes.AuthorWasNotUpdated:
+                    MessageBox.Show(_errorMessage.AuthorWasNotUpdated);
+                    break;
+                case ErrorsTypes.AuthorWasDeletedBeforeOrNotExitsAtAll:
+                    MessageBox.Show(_errorMessage.AuthorWasDeletedBeforeOrNotExitsAtAll);
+                    break;
+                case ErrorsTypes.NewBookWasNotCreated:
+                    MessageBox.Show(_errorMessage.NewBookWasNotCreated);
+                    break;
+                case ErrorsTypes.BookWasNotUpdated:
+                    MessageBox.Show(_errorMessage.BookWasNotUpdated);
+                    break;
+                case ErrorsTypes.NewAuthorWasNotCreated:
+                    MessageBox.Show(_errorMessage.NewAuthorWasNotCreated);
+                    break;
+                case ErrorsTypes.BookWasDeletedBeforeOrNotExitsAtAll:
+                    MessageBox.Show(_errorMessage.BookWasDeletedBeforeOrNotExitsAtAll);
+                    break;
+            }
         }
 
         public void ShowInfoMessage(string infoMessage)
         {
             MessageBox.Show(infoMessage);
         }
+
+
     }
 }
